b59b5a406d75f5b6a6d052e3dcea17e3c15e55d4
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2016-05-03 19:35:46 +0200

    check_syntax frame

--- shell.php
+++ shell.php
@@ -1,6 +1,11 @@
 <?php
 // Copyright (C) 2016 Grzegorz Kowalski, see LICENSE file
 
+function check_syntax($code) {
+	// execute php -a -l
+	return true;
+}
+
 while (true) {
 	$command_line = fgets(STDIN);
 	eval("$command_line;");
